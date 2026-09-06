3382b22068f1d868871fb3d1181b28675b77199e
warmmilkuh <warmmilkuh@gmail.com>
2026-09-07 01:44:32 +0800

    Add files via upload

---
+++ src-20260906T083115Z-1-001/src/App.jsx
@@ -0,0 +1,27 @@
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Contact from "./pages/Contact"
+import { Routes, Route } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="/about"
+          element={<About />}
+        />
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+      </Routes>
+    </>
+  )
+}
+
+export default App
